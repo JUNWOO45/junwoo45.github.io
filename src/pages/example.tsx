45b60a45e5d126560dda57119250401f89f74b53
junwoo45 <sinyoorang@gmail.com>
2022-05-15 15:08:28 +0900

    chore: 필요없는 파일 제거

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const ExamplePage = (): JSX.Element => <h1>Sample content</h1>;
+const ExamplePage = (): JSX.Element => <h1>hi Sample content</h1>;
 
 export default ExamplePage;
